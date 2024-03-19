5e2cd931c82c7c659afa81081d6736bdc933e67f
Z01berg <zaharzubik@gmail.com>
2024-03-19 19:35:57 +0100

    feature_avg

--- APBD/Program.cs
+++ APBD/Program.cs
@@ -2,6 +2,20 @@
 {
     public static void Main(string[] args)
     {
-        ///
+        int[] numbers = new[] { 1, 2, 3, 55, 56, 78 };
+        Console.Write($"AVERAGE: {feature_avg(numbers)}\n");
+    }
+    
+    public static int feature_avg(int[] numbers)
+    {
+        int sum = 0;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            sum += numbers[i];
+        }
+
+        int avg = sum / numbers.Length;
+    
+        return avg;
     }
 }
