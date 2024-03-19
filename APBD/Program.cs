d11ef6f486f52920f048bbbb98035b55e09b7cc6
Z01berg <zaharzubik@gmail.com>
2024-03-19 19:40:43 +0100

    feature-max

--- APBD/Program.cs
+++ APBD/Program.cs
@@ -3,7 +3,8 @@
     public static void Main(string[] args)
     {
         int[] numbers = new[] { 1, 2, 3, 55, 56, 78 };
-        Console.Write($"AVERAGE: {feature_avg(numbers)}\n");
+        Console.WriteLine($"AVERAGE: {feature_avg(numbers)}\n");
+        Console.WriteLine($"MAX: {feature_max(numbers)}\n");
     }
     
     public static int feature_avg(int[] numbers)
@@ -17,5 +18,19 @@
         int avg = sum / numbers.Length;
     
         return avg;
+    }
+    
+    public static int feature_max(int[] numbers)
+    {
+        int max = 0;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] > max)
+            {
+                max = numbers[i];
+            }
+        }
+        
+        return max;
     }
 }
